7b4396d71f6e302b222660fea02f6800c2f8402e
Valqueline Nicacio da silva <line.nicaco@hotmail.com>
2025-09-14 16:38:43 -0300

    chore: initial project setup

---
+++ src/test/java/com/nicacio/devicesapi/DevicesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicacio.devicesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevicesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
